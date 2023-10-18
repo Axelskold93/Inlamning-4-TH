451f696c5432f24f8dfe42c8ba0115e7190a9d0e
gabrielwedin <gabbe_wedin@hotmail.com>
2023-10-18 11:46:16 +0200

    Update Program.cs

--- Inlämning 4 TH/Program.cs
+++ Inlämning 4 TH/Program.cs
@@ -211,8 +211,6 @@ namespace Vaccination
                 vaccinationOrder.AddRange(children);
             }
 
-            vaccinationOrder = vaccinationOrder.OrderBy(person => person.IdNumber).ToList();
-
             return vaccinationOrder;
 
         }
