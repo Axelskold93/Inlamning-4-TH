01cc340091d5f9add11db699aa2475b07c1ad939
Axelskold93 <142592414+Axelskold93@users.noreply.github.com>
2023-10-23 13:31:38 +0200

    Included error handling in ReadCSVFile

--- Inlämning 4 TH/Program.cs
+++ Inlämning 4 TH/Program.cs
@@ -76,26 +76,26 @@ namespace Vaccination
             });
             if (option == 0)
             {
-                ReadCSVFile();
+
                 CreateVaccinationOrder(listOfPeople, vaccineDoses, vaccinateChildren);
-                SaveCSVFile(vaccinationOrder, outputFilePath);        
+                SaveCSVFile(vaccinationOrder, outputFilePath);
             }
-            else if(option == 1)
+            else if (option == 1)
             {
                 Console.Clear();
-                ChangeVaccinDoses();         
+                ChangeVaccinDoses();
             }
             else if (option == 2)
             {
-               vaccinateChildren = ChangeAgeLimit();
+                vaccinateChildren = ChangeAgeLimit();
             }
             else if (option == 3)
-            {           
-                ChangeInputCSVFilePath();        
-            }  
+            {
+                ChangeInputCSVFilePath();
+            }
             else if (option == 4)
             {
-                
+
                 ChangeOutputCSVFilePath();
             }
             else if (option == 5)
@@ -111,33 +111,89 @@ namespace Vaccination
             return IdNumber <= eighteenYears;
         }
         public static List<Person> ReadCSVFile()
-        {    
+        {
             Console.Clear();
-            string[] people = File.ReadAllLines(inputFilePath);
-            foreach (string l in people)
+            List<string> errorMessages = new List<string>();
+
+            try
             {
-                string[] values = l.Split(',');
-                string[] IdNumberParts = values[0].Split('-');
-                string birthYear = IdNumberParts[0];
-                DateTime idNumber = DateTime.ParseExact(birthYear, "yyyyMMdd", null);
-                TimeSpan years = DateTime.Today.Subtract(idNumber);
-                string lastFour = IdNumberParts[1];
-                double age = Math.Round(years.TotalDays / 365);
-                string lastName = values[1];
-                string firstName = values[2];
-                int healthCarePro = int.Parse(values[3]);
-                int highRisk = int.Parse(values[4]);
-                bool infected = values[5] == "1";
+                string[] people = File.ReadAllLines(inputFilePath);
+                foreach (string l in people)
+                {
+                    string[] values = l.Split(',');
+                    if (values.Length != 6) 
+                    {
+                        errorMessages.Add("Fel: Otillräckligt antal kolumner.");
+                    }
+                    string[] IdNumberParts = values[0].Split('-');
+                    if (IdNumberParts.Length != 2)
+                    {
+                        errorMessages.Add("Fel: Ogiltigt format på personnummer.");
+                    }
+                    string birthYear = IdNumberParts[0];
+                    DateTime idNumber;
+                    
+                    if (!DateTime.TryParseExact(birthYear, "yyyyMMdd", null, DateTimeStyles.None, out idNumber))
+                    {
+                        errorMessages.Add("Felaktigt format på födelsedatum.");
+                    }
+                    
+                    TimeSpan years = DateTime.Today.Subtract(idNumber);
+                    string lastFour = IdNumberParts[1];
+                    double age = Math.Round(years.TotalDays / 365);
+                    string lastName = values[1];
+                    if (lastName == "") 
+                    {
+                        errorMessages.Add("Kolumnen för efternamn är tom.");
+                    }
+                    string firstName = values[2];
+                    if (firstName == "")
+                    {
+                        errorMessages.Add("Kolumnen för förnamn är tom.");
+                    }
+                    int healthCarePro = int.Parse(values[3]);
+                    if (values[3] != "0" || values[3] != "1")
+                    {
+                        errorMessages.Add("Fel: Ogiltig siffra för Vård/Omsorg.");
+                    }
+                    int highRisk = int.Parse(values[4]);
+                    if (values[4] != "0" || values[4] != "1")
+                    {
+                        errorMessages.Add("Fel: Ogiltig siffra för Riskgrupp.");
+                    }
+                    bool infected = values[5] == "1";
+                    if (values[5] != "0" || values[5] != "1")
+                    {
+                        errorMessages.Add("Fel: Ogiltig siffra för Infekterad.");
+                    }
+
+
+                    Person person = new Person(idNumber, lastFour, lastName, firstName, healthCarePro, highRisk, infected, 0);
+                    listOfPeople.Add(person);
+                }
                 
-                Person person = new Person(idNumber,lastFour, lastName, firstName, healthCarePro, highRisk, infected, 0);
-                listOfPeople.Add(person);
             }
-            return listOfPeople;   
+            catch (Exception e)
+            {
+                errorMessages.Add(e.Message);           
+            }
+
+            if (errorMessages.Count > 0)
+            {
+                foreach (string error in errorMessages)
+                {
+                    Console.WriteLine(error);
+                }
+                Console.ReadKey();
+                MainMenu();
+            }
+
+            return listOfPeople;
         }
         public static string ChangeInputCSVFilePath()
         {
             Console.Clear();
-           
+
             while (true)
             {
                 Console.WriteLine("Vänligen ange sökväg:");
@@ -164,7 +220,7 @@ namespace Vaccination
                 string filePath = Console.ReadLine();
 
                 string directoryPath = Path.GetDirectoryName(filePath);
-               
+
 
                 if (Directory.Exists(directoryPath))
                 {
@@ -177,23 +233,24 @@ namespace Vaccination
                 {
                     Console.WriteLine("Hittar inte mapp. Mappen kan inte hittas på den angivna sökvägen.");
                 }
-            }  
+            }
         }
         public static void PrintList(List<Person> listOfPeople)
         {
             string eachPerson = "";
             foreach (Person person in listOfPeople)
             {
-                
-               eachPerson += ($"Person ID: {person.IdNumber} Name: {person.LastName} {person.FirstName} Healthcare professional: {person.HealthCarePro}\n");
+
+                eachPerson += ($"Person ID: {person.IdNumber} Name: {person.LastName} {person.FirstName} Healthcare professional: {person.HealthCarePro}\n");
             }
             Console.WriteLine(eachPerson);
             Console.ReadKey();
         }
         public static List<Person> CreateVaccinationOrder(List<Person> listOfPeople, int vaccineDoses, bool vaccinateChildren)
         {
-            List<Person> eligiblePeople = new List<Person> ();
-            
+            ReadCSVFile();
+            List<Person> eligiblePeople = new List<Person>();
+
             if (!vaccinateChildren)
             {
                 foreach (Person person in listOfPeople)
@@ -203,7 +260,7 @@ namespace Vaccination
                     {
                         eligiblePeople.Add(person);
                     }
-                    
+
                 }
             }
             else
@@ -211,34 +268,34 @@ namespace Vaccination
                 eligiblePeople = listOfPeople;
             }
 
-                var healthCareWorker = eligiblePeople
-                    .Where(person => person.HealthCarePro == 1)
-                    .OrderBy(person => person.IdNumber)
-                    .ToList();
-                vaccinationOrder.AddRange(healthCareWorker);
+            var healthCareWorker = eligiblePeople
+                .Where(person => person.HealthCarePro == 1)
+                .OrderBy(person => person.IdNumber)
+                .ToList();
+            vaccinationOrder.AddRange(healthCareWorker);
 
-                var over65 = eligiblePeople
-                    .Where(person => person.Age >= 65)
-                    .Except(healthCareWorker)
-                    .OrderBy(person => person.IdNumber)
-                    .ToList();
-                vaccinationOrder.AddRange(over65);
+            var over65 = eligiblePeople
+                .Where(person => person.Age >= 65)
+                .Except(healthCareWorker)
+                .OrderBy(person => person.IdNumber)
+                .ToList();
+            vaccinationOrder.AddRange(over65);
 
-                var riskGroup = eligiblePeople
-                    .Where(person => person.HighRisk == 1)
-                    .Except(healthCareWorker)
-                    .Except(over65)
-                    .OrderBy(person => person.IdNumber)
-                    .ToList();
-                vaccinationOrder.AddRange(riskGroup);
+            var riskGroup = eligiblePeople
+                .Where(person => person.HighRisk == 1)
+                .Except(healthCareWorker)
+                .Except(over65)
+                .OrderBy(person => person.IdNumber)
+                .ToList();
+            vaccinationOrder.AddRange(riskGroup);
 
-                var restOfPopulation = eligiblePeople
-                    .Except(healthCareWorker)
-                    .Except(over65)
-                    .Except(riskGroup)
-                    .OrderBy(person => person.IdNumber)
-                    .ToList();
-                vaccinationOrder.AddRange(restOfPopulation);
+            var restOfPopulation = eligiblePeople
+                .Except(healthCareWorker)
+                .Except(over65)
+                .Except(riskGroup)
+                .OrderBy(person => person.IdNumber)
+                .ToList();
+            vaccinationOrder.AddRange(restOfPopulation);
 
             foreach (var person in vaccinationOrder)
             {
@@ -259,63 +316,79 @@ namespace Vaccination
             }
 
             return vaccinationOrder;
-            
+
         }
-        public static void SaveCSVFile(List<Person>vaccinationOrder, string OutputFilePath)
+        public static void SaveCSVFile(List<Person> vaccinationOrder, string OutputFilePath)
         {
             Console.Clear();
             var lines = new List<string>();
-                foreach (var person in vaccinationOrder)
+
+            foreach (var person in vaccinationOrder)
+            {
+                string line = $"{person.IdNumber:yyyyMMdd}-{person.LastFour},{person.LastName},{person.FirstName},{person.GivenDoses}";
+                lines.Add(line);
+            }
+
+            if (File.Exists(OutputFilePath))
+            {
+                int option = ShowMenu("En fil existerar redan, vill du skriva över den?", new[]
                 {
-                 string line = $"{person.IdNumber:yyyyMMdd}-{person.LastFour},{person.LastName},{person.FirstName},{person.GivenDoses}";
-                 lines.Add(line);
+                    "Ja",
+                    "Nej"
+                });
+                if (option == 0)
+                {
+                    File.WriteAllText(outputFilePath, string.Empty);
+                    File.WriteAllLines(outputFilePath, lines);
+                    Console.WriteLine($"Resultatet har sparats i {outputFilePath}.");
+                    Console.ReadKey();
                 }
 
-                if (File.Exists(OutputFilePath))
-                {//lägg till meny
-                 File.WriteAllLines(outputFilePath, lines);
-                 Console.WriteLine($"Resultatet har sparats i {outputFilePath}.");
-                 Console.ReadKey();
-                }
-                else
-                {
-                 File.Create(OutputFilePath);
-                 Console.WriteLine("Fil skapad.");
-                }        
+
+            }
+            else
+            {
+                File.Create(OutputFilePath).Close();
+                Console.WriteLine("Fil skapad.");
+                File.WriteAllLines(outputFilePath, lines);
+                Console.WriteLine($"Resultatet har sparats i {outputFilePath}");
+                Console.ReadKey();
+            }
+
         }
         public static int ChangeVaccinDoses()
         {
             Console.Clear();
             int changingDoses = 0;
             Console.WriteLine($"Antal tillgängliga doser: {vaccineDoses}");
-            
+
             while (true)
             {
                 Console.WriteLine("Hur många doser vill du ändra till?");
                 try
-                {     
-                  changingDoses = int.Parse(Console.ReadLine());
+                {
+                    changingDoses = int.Parse(Console.ReadLine());
                     vaccineDoses = changingDoses;
                     Console.Clear();
                     Console.WriteLine($"Tillgängliga doser har ändrats till {changingDoses}.");
                     Console.ReadKey();
-                    return vaccineDoses;                   
+                    return vaccineDoses;
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine("Var god skriv in ett heltal.");
-                    Console.WriteLine($"Error: {e.Message}");       
+                    Console.WriteLine($"Error: {e.Message}");
                 }
-                
+
             }
 
-            
+
         }
 
         public static bool ChangeAgeLimit()
         {
             Console.Clear();
-            
+
             int option = ShowMenu("Ska personer under 18 vaccineras?", new[]
             {
                 "Ja",
@@ -411,5 +484,5 @@ namespace Vaccination
         #endregion
     }
 
-    
+
 }
